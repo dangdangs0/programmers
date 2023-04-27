99d1fee9f4e5e4ae69e8b900b77e645288c966e4
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-04-27 18:24:44 +0900

    Create 옹알이1.java

---
+++ Level 0/옹알이1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        boolean once=false;
+        // String[] speak={"aya","ye","woo","ma",
+        //                 "ayaye","ayawoo","ayama","ayayewoo","ayawooye",""
+        //                 "yeaya","yewoo","yema",
+        //                 "wooaya","wooye","wooma",
+        //                 "maaya","maye","mawoo",
+        //                 }
+        
+        int count=0;
+        for(int i=0;i<babbling.length;i++){
+            String temp=babbling[i];
+            if(temp.contains("aya")){
+                temp=temp.replaceFirst("aya"," ");
+                once=true;
+            }
+            if(temp.contains("ye")){
+                temp=temp.replaceFirst("ye"," ");
+                once=true;
+            }
+            if(temp.contains("woo")){
+                temp=temp.replaceFirst("woo"," ");
+                once=true;
+            }
+            if(temp.contains("ma")){
+                temp=temp.replaceFirst("ma"," ");
+                once=true;
+            }
+            
+            if(once){
+                temp=temp.replace(" ","");
+                if(temp.equals("")){//옹알이~
+                    count++;   
+                }
+            }
+            once=false;
+            
+        }
+        answer=count;
+        return answer;
+    }
+}
