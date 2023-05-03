cf905e1853c0274afa60e3a0428f8177024c6844
dangdang <52357071+dangdangs0@users.noreply.github.com>
2023-05-03 18:41:24 +0900

    Create 콜라츠추측.java

---
+++ Level 1/콜라츠추측.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n=(long)num;
+       
+        while(n!=1){
+            if(n%2==0){
+                n=n/2;
+                answer++;
+            }
+            else if(n%2!=0){
+                n=n*3+1;
+                answer++;
+            }
+            
+            if(n==1){
+                break;
+            }
+            
+            
+            if(answer>=500){
+                answer=-1;
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
